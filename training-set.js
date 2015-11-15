f035ad3d0d67e565efeb351e6180697dcb5f0375
Rowan Crawford <wombleton@gmail.com>
2015-11-15 18:56:20 +1300

    Slurp training data from mongo

---
+++ training-set.js
@@ -0,0 +1,68 @@
+require('babel/register');
+const _ = require('lodash');
+const config = require('./server/config/environment');
+const mongoose = require('mongoose');
+mongoose.connect(config.mongo.uri, config.mongo.options);
+const fs = require('fs');
+const async = require('async');
+const Tile = require('./server/api/tile/tile.model');
+const pixels = require('get-pixels');
+
+const trainLabels = fs.createWriteStream('train-labels.txt');
+const trainData = fs.createWriteStream('train-data.txt');
+const testLabels = fs.createWriteStream('test-labels.txt');
+const testData = fs.createWriteStream('test-data.txt');
+
+var count = 0;
+
+const q = async.queue(function (tile, callback) {
+  const test = _.random(1, 10) === 10;
+
+  const label = test ? testLabels : trainLabels;
+  const data = test ? testData : trainData;
+  label.write(tile.verdict + '\n');
+  console.log('Getting pixels for %s. %s tiles left to process.', tile.url, q.length());
+  pixels(tile.url, function (err, arr) {
+    if (err) {
+      console.log(err);
+      return callback(err);
+    }
+    data.write(arr.data.toString('base64') + '\n');
+    callback(count++);
+  });
+}, 10);
+
+q.drain = function () {
+  console.log('Output %s tiles.', count);
+};
+
+function fillQueue (hash) {
+  Tile.find({
+    verdict: {
+      $in: ['bee', 'notbee']
+    },
+    hash: {
+      $gt: hash
+    }
+  })
+  .limit(50)
+  .select('hash url verdict')
+  .sort('hash')
+  .exec(function (err, tiles) {
+    if (err) {
+      console.log(err);
+      return process.exit(1);
+    }
+    _.each(tiles, function (tile) {
+      q.push(tile);
+    });
+
+    if (tiles.length) {
+      setTimeout(function () {
+        fillQueue(_.last(tiles).hash);
+      }, 0);
+    }
+  });
+}
+
+fillQueue('');
